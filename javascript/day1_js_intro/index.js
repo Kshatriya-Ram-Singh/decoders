736fd427bee3b02a92a5d32ed1ba922c546ab0cc
22335A0512 <125196436+22335A0512@users.noreply.github.com>
2025-08-24 17:06:34 +0530

    day1

---
+++ javascript/day1_js_intro/index.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+document.write('<h1>welcome to javascript</h1>');
+let name=prompt("Enter your name:");
+document.write('<b>you have entered your name as:</b>' + name);
+
+let a=10;
+let b=20;
+let c=a+b;
+console.log(c);
